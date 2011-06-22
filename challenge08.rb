d8abdf37937c224ce115b4b0f60b52d4487a5664
Manuel Flores <manuelfloresv@gmail.com>
2011-06-22 19:03:51 +0200

    Use indexes, now is twice as fast.

--- challenge08.rb
+++ challenge08.rb
@@ -2,12 +2,16 @@
 # Author: Manuel Flores <manuelfloresv[at]gmail[.]com>
 # Usage: ruby challenge08.rb
 
-def longest_seq(dna1, dna2, match="")
+def longest_seq(dna1, dna2, index1=0, index2=0, match="")
   longest=match
   "ctag".each_char do |x|
-    if dna1.include? match+x and dna2.include? match+x
-      xlongest = longest_seq(dna1, dna2, match+x)
-      longest = xlongest if xlongest.size > longest.size
+    i1 = dna1.index(match+x, index1)
+    if i1
+      i2 = dna2.index(match+x, index2)
+      if i2
+        xlongest = longest_seq(dna1, dna2, i1, i2, match+x)
+        longest = xlongest if xlongest.size > longest.size
+      end
     end
   end
   return longest
